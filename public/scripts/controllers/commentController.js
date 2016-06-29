189c3faa8ed12d419e204203252a861aa1098697
AcaFaca <aca.alexandar@gmail.com>
2016-06-29 21:44:18 +0200

    report page layouting in progress

--- public/scripts/controllers/commentController.js
+++ public/scripts/controllers/commentController.js
@@ -58,43 +58,6 @@ angular.module('myApp').controller('commentController',
                 }
             }
 
-            self.showEditCommentForm = function (comment) {                
-               
-                if (comment == null) {
-                    comment = oldComment;
-                } else {
-                    oldComment = comment;
-                }
-                
-                $('p.comment-text').replaceWith($('p.comment-text'));
-                $('p.comment-text').replaceWith("<textarea name='edit-comment-field' ng-model='cc.commentText'>"+  comment.text + "</textarea>");
-                $('button[name="edit-comment-btn"]').replaceWith($('button[name="edit-comment-btn"]'));
-                $('button[name="edit-comment-btn"]').html("Save");
-                $('button[name="edit-comment-btn"]').attr("ng-click", "cc.editComment(comment)");
-                $('button[name="edit-comment-btn"]').after("<button name='cancel-edit-comment-btn' class='btn btn-default' ng-click='cc.hideEditCommentForm()'>Cancel</button>")
-                
-                $compile($(comment))($scope);
-                $compile($('p.comment-text'))($scope);
-                $compile($('button[name="edit-comment-btn"]'))($scope);
-                $compile($('button[name="cancel-edit-comment-btn"]'))($scope);
-            }
-
-            self.hideEditCommentForm = function (comment) {
-                $('textarea[name="edit-comment-field"]').replaceWith($('textarea[name="edit-comment-field"]'));
-                $('textarea[name="edit-comment-field"]').replaceWith("<p class='comment-text'>"+ oldComment.text + "</p>");
-                $compile($('textarea[name="edit-comment-field"]'))($scope);
-
-                oldComment.text = $("p.comment-text").text();
-                comment = oldComment;
-
-                $('button[name="edit-comment-btn"]').replaceWith($('button[name="edit-comment-btn"]'));
-                $('button[name="edit-comment-btn"]').html("Edit");
-                $('button[name="edit-comment-btn"]').attr("ng-click", "cc.showEditCommentForm(comment)");
-                $compile($('button[name="edit-comment-btn"]'))($scope);
-
-                $('button[name="cancel-edit-comment-btn"]').remove();
-            }
-
             self.editComment = function (comment) {
                 CommentService.editComment(comment)
                     .then(function (response) {
